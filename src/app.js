42ba5a7ee3af78baf684146d44540d8f2601e353
Whooolia <tpdms2779@gmail.com>
2018-12-20 11:09:45 +0000

    Refactor router

--- src/app.js
+++ src/app.js
@@ -11,15 +11,14 @@ const app = express();
 
 //express config
 //app.disable('x-powered-by');
-app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+app.set('port', process.env.PORT || 4040);
 //app.use(favicon(path.join(__dirname, '..', 'public','favicon.icon')))
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
-app.use(
-    express.static(path.join(__dirname, '..', 'public'),{maxAge:'30d'}
-));
+app.use(express.static(path.join(__dirname, '..', 'public')));
 
-app.use(controllers);
 
 app.engine(
     'hbs',
@@ -31,5 +30,10 @@ app.engine(
     })
 );
 
+app.get('/', (req, res) => {
+    res.render('home')
+});
+
+app.use(controllers);
 
 module.exports = app;
--- src/controllers/index.js
+++ src/controllers/index.js
@@ -2,8 +2,10 @@ const express = require('express');
 const path = require('path');
 const router = express.Router();
 
+const welcome = require('./welcome');
 const error = require('./error');
 
+router.get('/welcome', welcome.get);
 router.use(error.client);
 router.use(error.server);
 
