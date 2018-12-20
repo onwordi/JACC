739873c86752c50155b1d6951b5f5fee2013c693
aterna01 <ssaniok90@mail.ru>
2018-12-20 13:03:43 +0000

    new db

--- db/db_connection.js
+++ db/db_connection.js
@@ -1,12 +1,15 @@
 const { Pool } = require("pg");
 const url = require("url");
+
 const env = require("env2");
 env("./config.env");
 
-if (!process.env.DB_URL) {
+if (!process.env.DATABASE_URL) {
   throw new Error("Environment variable DATABASE_URL must be set");
 }
-const params = url.parse(process.env.DB_URL);
+
+const params = url.parse(process.env.DATABASE_URL);
+
 const [username, password] = params.auth.split(":");
 
 const options = {
@@ -15,12 +18,14 @@ const options = {
   database: params.pathname.split("/")[1],
   max: process.env.DB_MAX_CONNECTIONS || 2
 };
-// if (username) {
-//   options.user = username;
-// }
-// if (password) {
-//   options.password = password;
-// }
 
-// options.ssl = options.host !== "localhost";
+if (username) {
+  options.user = username;
+}
+if (password) {
+  options.password = password;
+}
+
+options.ssl = options.host !== "localhost";
+
 module.exports = new Pool(options);
