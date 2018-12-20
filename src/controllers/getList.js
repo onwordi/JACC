f12c60f257ea0ef61b906d4ce5c930e021ddf802
Whooolia <tpdms2779@gmail.com>
2018-12-20 16:52:57 +0000

    Add shoppinglist to the database

--- src/controllers/getList.js
+++ src/controllers/getList.js
@@ -3,21 +3,28 @@ const path = require("path");
 exports.post = (req, res) => {
   const name = req.body.name;
   // console.log(req.body.name);
+  const item = req.body.item;
+  const quantity = req.body.quantity;
+  const price = req.body.price;
 
   dbConnection.query(
     // return personId which contains people_id - to be passed in to bookings table
-    "INSERT INTO users (full_name) VALUES ($1)",
+    "INSERT INTO users (full_name) VALUES ($1) RETURNING id",
     [name],
-    (err, success) => {
+    (err, userRes) => {
       // console.log(personId); // logs out object for person, passed into bookings
-      if (err) {
-        return err;
-      } else {
-        // cb(null);
-        console.log("inserted into data");
-      }
-    }
-  );
+      if (err) return err;
+      dbConnection.query(
+    // return personId which contains people_id - to be passed in to bookings table
+    "INSERT INTO shoppinglist (user_id, item, quantity, price) VALUES ($1, $2, $3, $4)",
+    [userRes.rows[0].id, item, quantity, price],
+    err => {
+      // console.log(personId); // logs out object for person, passed into bookings
+      console.log("inserted into shoppinglist database");
+      if (err) return err;
+      console.log('error!!!', err)
+    });
+  });
 };
 
 exports.get = (req, res) => {
