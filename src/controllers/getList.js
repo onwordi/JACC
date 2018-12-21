ec31fded7657d7363bc83e6d7a86e3f0f6e018f4
Cynthia Onwordi <cynthiaonwordi@Cynthias-MacBook-Pro.local>
2018-12-21 11:36:08 +0000

    updated getlist

--- src/controllers/getList.js
+++ src/controllers/getList.js
@@ -1,37 +1,36 @@
 const dbConnection = require("../../db/db_connection.js");
 const path = require("path");
-
 exports.post = (req, res) => {
   const name = req.body.name;
-  // console.log(req.body.name);
-  console.log("111", name);
+
+  const item = req.body.item;
+  const quantity = req.body.quantity;
+  const price = req.body.price;
 
   dbConnection.query(
-    // return personId which contains people_id - to be passed in to bookings table
-    "INSERT INTO users (full_name) VALUES ($1)",
+
+    "INSERT INTO users (full_name) VALUES ($1) RETURNING id",
     [name],
-    (err, success) => {
-      // console.log(personId); // logs out object for person, passed into bookings
-      if (err) {
-        return err;
-      } else {
-        // cb(null);
-        console.log("inserted into database");
-      }
-    }
-  );
-//   console.log("222", name);
+    (err, userRes) => {
 
-  res.redirect(`/current-list/${name}`);
-  console.log("333", name);
+      if (err) return err;
+      dbConnection.query(
 
-  return name;
+        "INSERT INTO shoppinglist (user_id, item, quantity, price) VALUES ($1, $2, $3, $4)",
+        [userRes.rows[0].id, item, quantity, price],
+        err => {
+
+          console.log("inserted into shoppinglist database");
+          if (err) return err;
+          console.log('error!!!', err)
+        });
+    });
 };
 
-// exports.get = (req, res) => {
-//   res.sendFile(path.join(__dirname, "..", "views", "currentList.hbs"));
-// };
-//
-// exports.get = (req, res) => {
-//   res.render("currentList");
-// };
+exports.get = (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "views", "currentList.hbs"));
+};
+
+exports.get = (req, res) => {
+  res.render("shoppingList");
+};
